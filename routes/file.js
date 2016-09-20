70c04aed01b474bc6171fd02e4bebfa36a32c2f9
mistkafka <mistkafka@gmail.com>
2016-09-20 23:24:09 +0800

    new: dev: system default photos

--- routes/file.js
+++ routes/file.js
@@ -2,9 +2,12 @@ let express = require('express');
 let path = require('path');
 let app = express();
 let multer = require('multer');
-const FILE_PATH = path.join(__dirname, '../public/uploads/photo');
-let uploader = multer({dest: FILE_PATH});
-// let uploader = multer({dest: 'uploads/photo'});
+const SRC_2_FILE_PATH_MAPPING = {
+	system: '../public/images',
+	user: '../public/uploads/photo'
+};
+const DEFAULT_FILE_PATH = SRC_2_FILE_PATH_MAPPING['user'];
+let uploader = multer({dest: path.join(__dirname, DEFAULT_FILE_PATH)});
 
 app.post('/photo', uploader.single('photo'), function (req, res) {
     console.log(req.file);
@@ -16,7 +19,10 @@ app.post('/photo', uploader.single('photo'), function (req, res) {
 
 app.get('/photo', function (req, res) {
     let photoId = req.query.photoId || req.body.photoId;
-    res.sendFile(path.join(FILE_PATH, photoId));
+	let photoSrc = req.query.photoSrc || req.body.photoSrc;
+	let filePath = SRC_2_FILE_PATH_MAPPING[photoSrc || 'user'] || DEFAULT_FILE_PATH;
+
+    res.sendFile(path.join(__dirname, filePath, photoId));
 });
 
 module.exports = app;
